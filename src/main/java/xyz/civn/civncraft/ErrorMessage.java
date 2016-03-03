2c13c3f09666704950cea72824458aab47aed9cf
CIVN <comet.rice.m@gmail.com>
2016-03-03 20:52:51 +0900

    Delete ErrorMessage.java

--- src/main/java/xyz/civn/civncraft/ErrorMessage.java
+++
@@ -1,7 +0,0 @@
-package xyz.civn.civncraft;
-
-public class ErrorMessage
-{
-	static String UP = C.RED + "Unnecessary parameters!";
-	static String FC = C.RED + "You can't run this command from console!";
-}
