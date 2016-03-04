c37c4521195658c6a89befd9a96157698b5338f3
CIVN <civn.cf.storage@gmail.com>
2016-03-05 02:59:45 +0900

    delete

--- src/main/java/xyz/civn/civncraft/Prefix.java
+++
@@ -1,9 +0,0 @@
-package xyz.civn.civncraft;
-
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Prefix extends JavaPlugin implements Listener
-{
-	public static String prefix = C.GRAY + "[" + C.B + C.AQUA + C.B + "CIVNCraft" + C.R + C.GRAY + "] ";
-}
