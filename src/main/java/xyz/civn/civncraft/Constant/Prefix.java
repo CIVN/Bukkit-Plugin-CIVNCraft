069e95754a736051ab7f5201fe26d6a8e2c66266
CIVN <comet.rice.m@gmail.com>
2016-03-05 14:02:27 +0900

    Update Prefix.java

--- src/main/java/xyz/civn/civncraft/Constant/Prefix.java
+++ src/main/java/xyz/civn/civncraft/Constant/Prefix.java
@@ -3,8 +3,6 @@ package xyz.civn.civncraft.Constant;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-
 public class Prefix extends JavaPlugin implements Listener
 {
 	public static String prefix = C.GRAY + "[" + C.B + C.AQUA + C.B + "CIVNCraft" + C.R + C.GRAY + "] ";
